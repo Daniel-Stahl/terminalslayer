87a4291d8fd5e002ea172a4d7827a20a0563c39e
Daniel Stahl <stahl@Daniels-MacBook-Pro.local>
2019-09-12 22:48:10 -0700

    implimenting battle mode

--- main.cpp
+++ main.cpp
@@ -8,10 +8,15 @@
 
 using namespace std;
 
+void FindOpponent(Slayer& opponent);
+void BattleMode(Slayer slayer, Slayer opponent);
+
 int main() {
     // Create Class
     Slayer slayer;
+    Slayer opponent;
     int playersChoice;
+    int menu;
     
     cout << "Welcome to Terminal Slayer!" << endl
          << "Choose a Class:" << endl
@@ -28,22 +33,25 @@ int main() {
     } else if (playersChoice == 3) {
         slayer.CreateSlayer(150, 50);
     }
-    
-    cout << "We found an opponent." << endl;
 
  //Select Fight, Rest, or Exit
     do {
-        int menu;
         
-        cout << "Choose wisely:" << endl;
-        cout << "1: Fight" << endl
+        cout << "Choose wisely:" << endl
+             << "1: Fight" << endl
              << "2: Rest" << endl
              << "3: Leave the arena" << endl;
         
         cin >> menu;
         
         if (menu == 1) {
-            cout << "Fight" << endl;
+            cout << "Looking for an opponent" << endl;
+            
+            FindOpponent(opponent);
+            
+            cout << "Get ready, your opponent is entering the ring." << endl;
+            
+            BattleMode(slayer, opponent);
         } else if(menu == 2) {
             cout << "Rest" << endl;
         }
@@ -51,10 +59,31 @@ int main() {
         
     
     } while (menu != 3);
-    
-    cout << "Player selected: " << slayer.HP << endl;
-    cout << "Foe swings: " << slayer.AttackOne() << "Player HP is: " << slayer.HP - slayer.AttackOne() << endl;
-    slayer.HP = slayer.HP - slayer.AttackOne();
-    cout << "Player HP is: " << slayer.HP << endl;
-    
+
+}
+
+void FindOpponent(Slayer& opponent) {
+    opponent.CreateSlayer(100, 30);
+}
+
+void BattleMode(Slayer slayer, Slayer opponent) {
+    do {
+        int attackChoice;
+        int hit = slayer.AttackOne();
+        
+        cout << "Which attack do you want to use?" << endl;
+        cout << "1: Attack 1" << endl
+             << "2: Attack 2" << endl;
+        
+        cin >> attackChoice;
+        
+        if (attackChoice == 1) {
+            cout << "You use Attack 1" << endl;
+            cout << hit << " to opponent" << endl;
+            
+            opponent.HP = opponent.HP - hit;
+            
+            cout << "Opponent HP is: " << opponent.HP << endl;
+        }
+    } while (opponent.HP > 0);
 }
